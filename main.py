a168ed2b5945836241a98b59ddc9895c95767c7c
Anna Khromchenkova <anya.petrova2013@bk.ru>
2020-10-05 15:49:58 +0500

    Task 2 is solved

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ if __name__ == '__main__':
     # Task 1
     arr = [1,2,3,4,5]
     print(arr[0], arr[2], arr[-2])
+
+    print()
+
+    # Task 2
+    arr = [1, 2, 3, 4, 5, 6, 7]
+    print('Введите число')
+    N = int(input())
+    if (len(arr) >= N and N > 0):
+        print(arr[N - 1] ** N)
+    else:
+        print(-1)
