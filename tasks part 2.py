6a5998c4beabcd28d43dace327da133b98f93418
Anna Khromchenkova <anya.petrova2013@bk.ru>
2020-12-08 23:24:57 +0500

    Task #2 from Part 2 is solved

--- tasks part 2.py
+++ tasks part 2.py
@@ -11,3 +11,17 @@ for i in string.ascii_uppercase:
         max = count
         letter = i
 print(max, '-', letter)
+
+# Task 2
+text = input()
+count = 0
+for i in text.split():
+    if i.isalpha():
+        count += 1
+    else:
+        count = 0
+    if count == 3:
+        print("есть последовательность из 3 слов")
+        break;
+if count < 3:
+    print ('отсутствует последовательность из 3 слов')
